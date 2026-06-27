ffe05e7b2b86d052f83a58d62a85b2c5283705df
Areeba Noor <areebanoor5687@gmail.com>
2026-06-27 18:26:37 +0500

    initial commit

---
+++ 01-Basics.py
@@ -0,0 +1,18 @@
+#task1:Print your name, university, and goal.
+name="Areeba Noor"
+university="University of Chakwal"
+
+print("My name is", name)
+print("My university name is " , university)
+print("My Goal is to be frontend Web developer then move toward Ai and dataScience")
+
+#task2 :Take the user's name and greet them.
+userName= input("Enter your name:")
+print("Hello", userName)
+
+#task3: Take two integers and print their sum, difference, and product.
+num1=int(input("Enter 1st number:"))
+num2=int(input("Enter 2nd number:"))
+print("Sum :",num1+num2)
+print("Difference:"num1-num2)
+print("Product:"num1*num2)
