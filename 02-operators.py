baa76891ac5408bef490d34e67fba8e7485aa6de
Areeba Noor <areebanoor5687@gmail.com>
2026-06-27 18:46:40 +0500

    intial commit:operators practice

---
+++ 02-operators.py
@@ -0,0 +1,28 @@
+# Task 1
+# Take two integers from the user and print:
+# Addition
+# Subtraction
+# Multiplication
+# Division
+# Floor Division
+# Modulus
+# Power
+
+num1=int(input("Enter 1st Number:"))
+num2=int(input("Enter 2nd Number:"))
+print("Addition :", num1+num2)
+print("Subtraction :", num1-num2)
+print("Multiplication :", num1*num2)
+print("Division :", num1/num2)
+print("Floor Division :", num1//num2)
+print("Modulus :", num1%num2)
+print("Power :", num1**num2)
+
+
+#comparison operator
+print(num1==num2)
+print(num1<num2)
+print(num1<=num2)
+print(num1>num2)
+print(num1>=num2)
+print(num1!=num2)
