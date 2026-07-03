53dd41293eddccdbc8240a2941897598eefafe0a
Areeba Noor <areebanoor5687@gmail.com>
2026-07-03 19:00:52 +0500

    feat: task search element

---
+++ 07-Lists/task7-searchElement.py
@@ -0,0 +1,19 @@
+# Task 
+
+# Search for a number.
+
+numbers=[23,67,89,32,14,56]
+
+target=int(input("Enter Number to find:\n"))
+
+found='false'
+
+for index,value in enumerate(numbers):
+    if (target==value):
+        print("Target ",value ," found at index ",index )
+        found='true'
+        break
+
+if  found:
+    print("Target value not found.")
+        
